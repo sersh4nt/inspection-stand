dd618e8ef9ba4c2d6750a13b6c2bc58734d9c3bd
vlad <rektf12@gmail.com>
2021-04-28 14:01:51 +0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import asyncio
+from aiohttp import web
+from typing import Any
+
+
+async def handle(request):
+    return web.Response(text="hello, world")
+
+
+def main(args: Any = None) -> None:
+    app = web.Application()
+    app.add_routes([web.get('/', handle)])
+    web.run_app(app)
+
+
+if __name__ == '__main__':
+    main()
